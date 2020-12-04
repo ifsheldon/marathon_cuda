9b3ae276b5e2464cab3b75b022c625557d4c0aa4
ifsheldon <maplevantablack@outlook.com>
2020-12-04 22:40:42 +0800

    added camera

---
+++ src/utils/camera.hpp
@@ -0,0 +1,33 @@
+//
+// Created by Maple on 2020/12/4.
+//
+
+#ifndef MARATHON_CUDA_CAMERA_HPP
+#define MARATHON_CUDA_CAMERA_HPP
+
+#include "glm/glm.hpp"
+
+using glm::vec3;
+using glm::mat4;
+
+struct Camera
+{
+    vec3 position;
+    vec3 up;
+    vec3 center;
+    mat4 look_at_mat;
+};
+
+struct CameraConfig
+{
+    float near;
+    float far;
+    float fovy_radian;
+};
+
+enum Mode
+{
+    Orbit, Zoom
+};
+
+#endif //MARATHON_CUDA_CAMERA_HPP
